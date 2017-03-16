5c358fcf9e4f36ec53096e588839f397ed950a33
Colette0527 <colette05@ufl.edu>
2017-03-16 11:50:19 -0400

    Update Deckdesigner.java

--- .idea/Deckdesigner.java
+++ .idea/Deckdesigner.java
@@ -2,46 +2,51 @@
  * Created by Colette on 3/15/2017.
  */
 public class Deckdesigner {
-    public Hex[][] Deckoftiles = new Hex[3][48];
+    int numberofterrainspertile=3;
+    int numberoftotaltiles = 48;
+    public Hex[][] Deckoftiles= new Hex[numberoftotaltiles][numberofterrainspertile];
     public int startofrangeoffirstterraintype;
-    public void tilecreator() {
+
+    public Hex[][] tilecreator() {
+    //    Deckoftiles= Deckcreator();
         for (int i = 0; i < 48; i++)
-            Deckoftiles[2][i]=new Hex('V');
+            Deckoftiles[i][2]=new Hex('V');
         for (int i = 0; i < 12; i++)
-            Deckoftiles[0][i] =new Hex('A');
+            Deckoftiles[i][0] =new Hex('A');
         startofrangeoffirstterraintype=0;
         secondvaluesetup();
         for (int i = 12; i < 24; i++)
-            Deckoftiles[0][i] = new Hex('B');
+            Deckoftiles[i][0] = new Hex('B');
         startofrangeoffirstterraintype=12;
         secondvaluesetup();
         for (int i = 24; i < 36; i++)
-            Deckoftiles[0][i] = new Hex('C');
+            Deckoftiles[i][0] = new Hex('C');
         startofrangeoffirstterraintype=24;
         secondvaluesetup();
         for (int i = 36; i < 48; i++)
-            Deckoftiles[0][i] =new Hex('S');
+            Deckoftiles[i][0] =new Hex('D');
         startofrangeoffirstterraintype=36;
         secondvaluesetup();
+        return Deckoftiles;
     }
     /* For the most part the deck is always set up in the same way where it sets up the 12 tiles that begin with with first
     * type (like A) then it sets up three of the second value in secondvaluesetup
     * so the deck will always be in the order of AAV AAV AAV ABV ABV ABV ACV ACV.... and so on*/
     private void secondvaluesetup(){
         for(int i=0; i<2; i++){
-            Deckoftiles[1][startofrangeoffirstterraintype]=new Hex('A');
+            Deckoftiles[startofrangeoffirstterraintype][1]=new Hex('A');
             startofrangeoffirstterraintype++;
         }
         for(int i=0; i<2; i++){
-            Deckoftiles[1][startofrangeoffirstterraintype]=new Hex('B');
+            Deckoftiles[startofrangeoffirstterraintype][1]=new Hex('B');
             startofrangeoffirstterraintype++;
         }
         for(int i=0; i<2; i++){
-            Deckoftiles[1][startofrangeoffirstterraintype]=new Hex('C');
+            Deckoftiles[startofrangeoffirstterraintype][1]=new Hex('C');
             startofrangeoffirstterraintype++;
         }
         for(int i=0; i<2; i++){
-            Deckoftiles[1][startofrangeoffirstterraintype]=new Hex('D');
+            Deckoftiles[startofrangeoffirstterraintype][1]=new Hex('D');
             startofrangeoffirstterraintype++;
         }
     }
