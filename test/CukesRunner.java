d85995edfa2c3daa7086960c299bd58af923458f
rmercker <merckerrobert@yahoo.com>
2017-03-16 23:46:51 -0400

    AcceptenceTest work
    # Conflicts:
    #	.idea/workspace.xml
    #	out/production/Game/Deck.class
    #	out/test/Game/CucumberGameBoardTest.class
    #	out/test/Game/Map.feature
    #	test/CucumberGameBoardTest.java
    #	test/Map.feature

--- test/CukesRunner.java
+++
@@ -1,9 +0,0 @@
-import cucumber.api.junit.Cucumber;
-import cucumber.api.CucumberOptions;
-import org.junit.runner.RunWith;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features={"test"}
-)
-public class CukesRunner {}
