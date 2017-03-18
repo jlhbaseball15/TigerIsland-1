1b6ff2d9d19430b309f5155b9e92f5d6b0bc4247
sadelarosa <sadelarosa@ufl.edu>
2017-03-18 01:38:30 -0400

    Added a new unit test, fixed deck order and spelling
    New unit test checks to make sure removing last tile from deck does not
    create any issues. The deck is now is now in the correct the order
    specified by the string.

--- UnitTest/DeckTest.java
+++ UnitTest/DeckTest.java
@@ -5,15 +5,18 @@ import org.junit.Test;
 public class DeckTest {
     private Deck deck;
     private Deck shuffledDeck;
+    private Deck DeckBecomesEmpty;
+
 
     @Before
-    public void CreatDeck() {
+    public void CreateDecks() {
         deck = new Deck();
         shuffledDeck = new Deck("JL GL");
+        DeckBecomesEmpty = new Deck("JG");
     }
 
     @Test
-    public void RecieveThreeHexes() {
+    public void ReceiveThreeHexes() {
         Tile tile = deck.getTile();
         Hex[] hex = tile.getHexes();
         Assert.assertEquals('G', hex[0].getTerrain());
@@ -21,11 +24,20 @@ public class DeckTest {
         Assert.assertEquals('V', hex[2].getTerrain());
     }
      @Test
-    public void ShuffledRecieveThreeHexes() {
+    public void ShuffledReceiveThreeHexes() {
         Tile tile = shuffledDeck.getTile();
         Hex[] hex = tile.getHexes();
-        Assert.assertEquals('G', hex[0].getTerrain());
+        Assert.assertEquals('J', hex[0].getTerrain());
         Assert.assertEquals('L', hex[1].getTerrain());
         Assert.assertEquals('V', hex[2].getTerrain());
     }
+    @Test
+    public void LastTileGetsDrawnFromDeck() {
+        Tile tile = DeckBecomesEmpty.getTile();
+        Hex[] hex = tile.getHexes();
+        Assert.assertEquals('J', hex[0].getTerrain());
+        Assert.assertEquals('G', hex[1].getTerrain());
+        Assert.assertEquals('V', hex[2].getTerrain());
+    }
+    
 }
