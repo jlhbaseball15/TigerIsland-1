e4d6db2ada041293bed03dd0dea389525a684884
Mathew Giaramita <m.giaramita@gmail.com>
2017-04-05 19:35:06 -0400

    Thread classes for TigerIslandClient Server->AI communication

---
+++ src/SenderThread.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class SenderThread implements Runnable{
+    private PrintWriter writer;
+//    private AI pA;
+//    private AI pB;
+
+    public SenderThread(PrintWriter writer /*, AI ai */){
+        this.writer = writer;
+        //set AI
+    }
+
+    @Override
+    public void run(){
+
+    }
+}
