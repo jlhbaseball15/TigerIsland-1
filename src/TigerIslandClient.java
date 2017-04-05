d62158971cbe405f68fc3f0f97e5c9ad2bf2f62d
sadelarosa <sadelarosa@ufl.edu>
2017-04-05 17:07:55 -0400

    non-hardcoded server info and password
    command line arguments

--- src/TigerIslandClient.java
+++ src/TigerIslandClient.java
@@ -1,8 +1,21 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 public class TigerIslandClient{
-	private static final String serverUrl = "localhost";
-	private static final int port = 8080;
 
 	public static void main(String[] args){
+		String serverUrl = args[0];
+		int port = Integer.parseInt(args[1]);
+		String tournamentPassword = args[2];
+		String userName = args[3];
+		String passWord = args[4];
+
+		if (args.length != 5) {
+			System.err.println(
+					"need <serverUrl> <port> <tournamentpassword> <username> <password>");
+			System.exit(1);
+		}
+
 		/*
 		try(
 			Socket         socket = new Socket(server_URL, port_number);
