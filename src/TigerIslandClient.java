99ce5405d4de4ff56d056b05d5986794a39581b7
Mathew Giaramita <m.giaramita@gmail.com>
2017-04-09 00:27:52 -0400

    Client might be done, needs testing...

--- src/TigerIslandClient.java
+++ src/TigerIslandClient.java
@@ -1,11 +1,23 @@
 import java.io.*;
 import java.net.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class TigerIslandClient{
 	private static String tournamentPassword, username, password;
-	private static AI aiP1, aiP2;
+	private static ServerToClientMessageAdaptor s2c;
+	private static ClientToServerMessageAdaptor c2s;
+	private static Queue<Message> p1InMessages, p1OutMessages, p2InMessages, p2OutMessages;
+	private static Thread aiP1, aiP2;
 
 	public static void main(String[] args){
+		s2c = new ServerToClientMessageAdaptor();
+		c2s = new ClientToServerMessageAdaptor();
+
+		p1InMessages = new LinkedList<Message>();
+		p1OutMessages = new LinkedList<Message>();
+		p2InMessages = new LinkedList<Message>();
+		p2OutMessages = new LinkedList<Message>();
 
 		String serverUrl = args[0];
 		int port = Integer.parseInt(args[1]);
@@ -14,7 +26,7 @@ public class TigerIslandClient{
 		password = args[4];
 
 		if (args.length != 5) {
-			System.err.println("need <serverUrl> <port> <tournamentpassword> <username> <password>");
+			System.err.println("TigerIslandClient <serverUrl> <port> <tournamentpassword> <username> <password>");
 			System.exit(1);
 		}
 
@@ -35,11 +47,11 @@ public class TigerIslandClient{
 	}
 
 	private static void authProtocol(PrintWriter writer, BufferedReader reader) throws IOException{
-		reader.readLine(); //Welcome to thunderdome
+		reader.readLine(); //WELCOME TO ANOTHER EDITION OF THUNDERDOME!
 		writer.println("ENTER THUNDERDOME " + tournamentPassword);
-		reader.readLine(); //Two enter one leave
+		reader.readLine(); //TWO SHALL ENTER, ONE SHALL LEAVE
 		writer.println("I AM " + username + " " + password);
-		reader.readLine(); //wait for tournament to begin...
+		reader.readLine(); //WAIT FOR THE TOURNAMENT TO BEGIN <pid>
 	}
 
 	private static void challengeProtocol(PrintWriter writer, BufferedReader reader) throws IOException {
@@ -69,8 +81,11 @@ public class TigerIslandClient{
 	private static void matchProtocol(PrintWriter writer, BufferedReader reader) throws IOException{
 		reader.readLine(); //NEW MATCH BEGINNING NOW YOUR OPPONENT IS PLAYER <pid>
 
-		aiP1 = new AI(true);
-		aiP2 = new AI(false);
+		aiP1 = new Thread(new AI(true, p1InMessages, p1OutMessages));
+		aiP2 = new Thread(new AI(false, p2InMessages, p2OutMessages));
+
+		aiP1.start();
+		aiP2.start();
 
 		moveProtocol(writer, reader);
 
@@ -80,11 +95,46 @@ public class TigerIslandClient{
 
 	private static void moveProtocol(PrintWriter writer, BufferedReader reader) throws IOException{
 		//MAKE YOUR MOVE IN GAME <gid> WITHIN <time move > SECONDS: MOVE <#> PLACE <tile>
-		//Create Message Object for it and Pass to correct AI
+		String message = reader.readLine();
+		p1InMessages.add(s2c.translate(message));
+		message = message.split(" ")[5];  //Game I just made a move in
 
 		//Poll for client messages and send whenever one is available
+		while(p1OutMessages.isEmpty());
+		writer.println(c2s.translate(p1OutMessages.remove()));
 
 		//Two messages from server. Pass to correct AI
 		//Move you just made (w/ legal confirmation) and move made by enemy in other game
+		String m1 = reader.readLine();
+		String m2 = reader.readLine();
+		if(m1.split(" ")[1].equals(message)){
+			p1InMessages.add(s2c.translate(m1));
+			p2InMessages.add(s2c.translate(m2));
+		}
+		else{
+			p2InMessages.add(s2c.translate(m1));
+			p1InMessages.add(s2c.translate(m2));
+		}
+
+		//make move other game
+		message = reader.readLine();
+		p2InMessages.add(s2c.translate(message));
+		message = message.split(" ")[5];
+
+		//poll 4 message other game
+		while(p2OutMessages.isEmpty());
+		writer.println(c2s.translate(p2OutMessages.remove()));
+
+		//two messages other game
+		m1 = reader.readLine();
+		m2 = reader.readLine();
+		if(m1.split(" ")[1].equals(message)){
+			p1InMessages.add(s2c.translate(m1));
+			p2InMessages.add(s2c.translate(m2));
+		}
+		else{
+			p2InMessages.add(s2c.translate(m1));
+			p1InMessages.add(s2c.translate(m2));
+		}
 	}
 }
